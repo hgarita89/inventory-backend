2dd6e2f2ad5d16149f4df2b46214a55860f61b22
hgarita89 <topohumbertogarita@gmail.com>
2024-03-30 13:53:23 -0600

    Servicio de buscar categoria por id

--- src/main/java/com/company/inventory/services/CategoryServiceImpl.java
+++ src/main/java/com/company/inventory/services/CategoryServiceImpl.java
@@ -1,6 +1,8 @@
 package com.company.inventory.services;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -33,6 +35,30 @@ public class CategoryServiceImpl implements ICategoryService {
 			response.setMetadata("Respuesta ok", "00", "Respuesta exitosa");
 		} catch (Exception e) {
 			response.setMetadata("Respuesta nok", "-1", "Error al consultar");
+			e.getStackTrace();
+			return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		
+		return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.OK);
+	}
+
+	@Override
+	@Transactional(readOnly = true) //Para indicar que este metodo no modificara la base de datos.
+	public ResponseEntity<CategoryResponseRest> searchByID(Long id) {
+		CategoryResponseRest response = new CategoryResponseRest();
+		List<Category> list = new ArrayList<>();
+		try {
+			Optional<Category> category = categoryDao.findById(id);
+			if (category.isPresent()) {
+				list.add(category.get());
+				response.getCategoryResponse().setCategory(list);
+				response.setMetadata("Respuesta ok", "00", "Categoria encontrada");
+			} else {
+				response.setMetadata("Respuesta nok", "-1", "Categoria no encontrada");
+				return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.NOT_FOUND);
+			}
+		} catch (Exception e) {
+			response.setMetadata("Respuesta nok", "-1", "Error al consultar por id");
 			e.getStackTrace();
 			return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.INTERNAL_SERVER_ERROR);
 		}
--- src/main/java/com/company/inventory/services/ICategoryService.java
+++ src/main/java/com/company/inventory/services/ICategoryService.java
@@ -5,6 +5,11 @@ import org.springframework.http.ResponseEntity;
 import com.company.inventory.response.CategoryResponseRest;
 
 public interface ICategoryService {
+	
+	// Método para buscar todas las categorías.
 	public ResponseEntity<CategoryResponseRest> search();
+	
+	// Método para buscar una categoría por su ID.
+	public ResponseEntity<CategoryResponseRest> searchByID(Long id);
 	
 }
